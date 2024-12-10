4a7898a0729cafb7a22ed5eec3238baa88c68527
Akshaya-1701 <akshayajureddy1482@gmail.com>
2024-12-10 09:05:30 -0600

    Add files via upload

---
+++ client/src/utils/resumedata.js
@@ -0,0 +1,158 @@
+export const resumedata1 = {
+    personalInfo: {
+      name: "Luna Thomas",
+      title: "Product Manager | Strategy & Innovation",
+      email: "luna.thomas@gmail.com",
+      phone: "469-585-7236",
+      location: "San Francisco, California",
+      linkedin: "linkedin.com/in/lunathomas",
+    },
+    summary:{
+      summary:"With over 3 years of experience in product management, I have a proven track record of driving product strategy and innovation. My expertise in data integration platforms, user experience, and agile methodologies has been pivotal in delivering successful products.",
+    },
+    experience: [
+      {
+        position: "Senior Product Manager",
+        company: "ABC Corp",
+        years: "2020 - Present",
+        location: "San Francisco, California",
+        description: [
+          "Led product strategy and execution for data integration platforms, resulting in a 25% increase in customer satisfaction.",
+          "Collaborated with cross-functional teams to launch new features, enhancing user experience and engagement.",
+          "Implemented agile methodologies to streamline product development, reducing time to market by 20%.",
+          "Conducted market research and competitive analysis, leading to a 15% growth in market share.",
+        ],
+      },
+      {
+        position: "Product Manager",
+        company: "XYZ Inc.",
+        years: "2018 - 2020",
+        location: "San Francisco, California",
+        description: [
+          "Managed end-to-end product development for a key feature, resulting in a 30% increase in user adoption.",
+          "Worked closely with engineering and design teams to deliver a seamless user experience.",
+          "Developed and maintained product roadmaps, ensuring alignment with business goals.",
+          "Conducted user research and usability testing to inform product decisions.",
+        ],
+      },
+      {
+        position: "Associate Product Manager",
+        company: "Tech Solutions",
+        years: "2016 - 2018",
+        location: "San Francisco, California",
+        description: [
+          "Supported senior product managers in the launch of three new products.",
+          "Assisted in the creation of product requirements and specifications.",
+          "Coordinated with marketing and sales teams to ensure successful product launches.",
+          "Conducted data analysis to identify trends and opportunities for product improvement.",
+        ],
+      },
+    ],
+    education: {
+      school: "University of California, Berkeley",
+      school2: "University of California, Berkeley",
+      degree1: "Master of Business Administration (MBA)",
+      degree1years: "2014 - 2016",
+      degree2: "Bachelor of Science in Computer Science",
+      degree2years: "2010 - 2014",
+    },
+    skills: [
+      "Product Management",
+      "Data Integration",
+      "Agile Methodologies",
+      "User Experience",
+    ],
+    certifications: [
+      "Certified Scrum Master (CSM)",
+      "Certified Product Manager (CPM)",
+    ],
+    achievements: [
+      "Launched Financial Data Platform, leading to a 30% increase in data accuracy.",
+      "Improved Customer Satisfaction by 25% through enhanced user experience.",
+      "Increased Transaction Volumes by 20% with streamlined processes.",
+      "Streamlined Development Process, reducing time to market by 20%.",
+    ],
+    languages: ["English", "Spanish", "Mandarin"],
+  }
+
+
+export const resumedata2 = {
+    personalInfo: {
+      name: "James Ryan",
+      title: " Graduate Student | Research & Development",
+      email: "james.ryan@gmail.com",
+      phone: "940-629-3847",
+      location: "Dallas, Texas",
+      linkedin: "linkedin.com/in/jamesryan",
+    },
+    summary: "Graduate student with a background in product management, skilled in data integration, user-centered design, and agile methodologies. Passionate about using analytics and creative problem-solving to deliver impactful, user-focused products.",
+    experience: [
+      {
+        position: "Product Management Intern",
+        company: "InnovateTech",
+        years: "Summer 2023",
+        location: "New York, New York",
+        description: [
+          "Assisted in developing the product strategy for a new SaaS platform, leading to a 20% improvement in user engagement post-launch.",
+          "Collaborated with UX and engineering teams to prioritize and launch key features, optimizing user experience and product functionality.",
+          "Conducted competitor analysis and user research, providing insights that shaped the product roadmap.",
+          "Supported agile development processes, improving sprint planning efficiency and reducing delivery times by 15%.",
+        ],
+      },
+      {
+        position: "Business Analyst Intern",
+        company: "Global Solutions Group",
+        years: "2022 - 2023",
+        location: "San Francisco, California",
+        description: [
+          "Analyzed market trends and customer data to recommend features for an enterprise software solution, which boosted client retention by 10%.",
+          "Worked with cross-functional teams to improve internal product workflows, enhancing productivity by standardizing processes.",
+          "Built dashboards for tracking product KPIs, allowing stakeholders to make data-driven decisions and align with business goals.",
+          "Conducted user testing and synthesized feedback, ensuring product design aligned with customer needs.",
+        ],
+      },
+      {
+        position: "Junior Product Analyst",
+        company: "Tech Innovations",
+        years: "2021 - 2022",
+        location: "Remote",
+        description: [
+          "Supported the development and launch of a mobile app feature, increasing user engagement by 15%.",
+          "Performed data analysis to understand usage patterns and improve user experience based on behavioral insights.",
+          "Assisted in drafting product requirements documents (PRDs), ensuring clarity for development and design teams.",
+          "Worked closely with customer support to analyze feedback and suggest improvements to the product roadmap.",
+        ],
+      },
+    ],
+    education: {
+      school: "Purdue University",
+      degree1: "Master of Science in Data Science",
+      degree1years: "2022 - 2024",
+      degree2: "Bachelor of Science in Electrical Engineering",
+      degree2years: "2018 - 2022",
+    },
+    skills: ["Product Management", "Market Research", "Data Analysis", "Competitive Analysis", "User Testing & Feedback Analysis", "Business Strategy", "Frameworks: Node.js, Express.js, Bootstrap", "Developer Tools: Git, VS Code, PyCharm, Jupyter, MATLAB, LabView, Multisim", "Libraries: pandas, NumPy, Matplotlib, scikit-learn", "Coding Languages: Python, C/C++, JavaScript, HTML/CSS, Matlab"],
+    certifications: ["Product Management Certificate (PMC)", "Project Management Professional (PMP)", "Certified Business Analysis Professional (CBAP)", "Google Analytics Individual Qualification (GAIQ)", "Data Analytics Certificate", "AWS Certified Cloud Practitioner"],
+    achievements: [
+      "Developed a Collaborative Research Tool, resulting in a 35% increase in team productivity.",
+      "Enhanced Data Analysis Techniques, improving project efficiency by 25%.",
+      "Designed and Implemented a User Feedback System, leading to a 30% boost in user engagement.",
+      "Spearheaded a Marketing Campaign, achieving a 40% growth in student participation in events.",
+    ],
+    responsibilitiesundertaken: [
+        "Career Development Representative - Actively participated in organizing career fairs and networking events for students in the Electrical Engineering department.",
+        "Student Engagement Coordinator - Collaborated with faculty and administration to enhance student participation in academic and extracurricular activities.",
+        "Event Management Team - Contributed to planning and executing various college events, including workshops and guest lectures.",
+        "Assisted in coordinating recruitment drives for local tech companies as part of the placement cell.",
+    ],
+    extracurricular: [
+        "Winner - Served as a team member in a case competition, leading to a first-place finish out of 10 teams at the Business Strategy Challenge 2023.",
+        "Participant - Contributed as a panelist in the university's Innovation Summit, sharing insights on technology trends with industry professionals.",
+        "Co-Founder - Co-founded a student-led organization focused on promoting sustainability initiatives on campus, resulting in a 25% increase in student participation in green activities.",
+        "Organizer - Led the organizing committee for the annual Tech Fest, coordinating events and workshops that attracted over 500 attendees from various disciplines.",
+        "Finalist - Achieved finalist status in the National Entrepreneurship Competition, presenting a business plan that garnered interest from several investors.",
+        "Volunteer - Volunteered as a mentor in the university's tutoring program, providing academic support and guidance to underclassmen in STEM subjects.",
+        "Speaker - Delivered a presentation on data analytics at the Graduate Research Symposium, receiving recognition for innovative research approaches.",
+    ],
+    languages: ["English", "French", "German"],
+  }
